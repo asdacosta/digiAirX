3c52a4ed53e49b04021694c4710640e366ffcd7f
asdacosta <asdacosta18@gmail.com>
2024-07-26 13:32:36 +0000

    Remove Load from parent component when animation ends

--- src/Load/Load.jsx
+++ src/Load/Load.jsx
@@ -1,12 +1,23 @@
 import { DotLottieReact as Lot } from "@lottiefiles/dotlottie-react";
 import styles from "./Load.module.css";
+import { useEffect, useState } from "react";
 
-function Load() {
+function Load({ endOnComplete }) {
+  const [lottie, setLottie] = useState(null);
+  const lottieRefCall = (lottie) => setLottie(lottie);
+
+  const endAndClearOnComplete = () => {
+    if (!lottie) return;
+    lottie.addEventListener("complete", endOnComplete);
+    return () => lottie.removeEventListener("complete", endOnComplete);
+  };
+  useEffect(endAndClearOnComplete, [lottie]);
+
   return (
     <section className={styles.load}>
       <Lot
         autoplay
-        loop
+        dotLottieRefCallback={lottieRefCall}
         src="https://raw.githubusercontent.com/asdacosta/digiAirX/main/src/assets/load.json"
         style={{ width: "20rem", height: "20rem" }}
       ></Lot>
