6a03215986b2e4e9daa7cee3d73911720759ffe5
asdacosta <asdacosta18@gmail.com>
2024-07-29 22:29:57 +0000

    Set routes with react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./Home/Home.jsx";
+import { About } from "./About/About.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
