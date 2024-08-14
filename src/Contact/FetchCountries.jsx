72819be311be1fbec30372111b7b4321a0f7cf68
asdacosta <asdacosta18@gmail.com>
2024-08-14 18:44:24 +0000

    Fetch countries from api, sort, and embed in options

---
+++ src/Contact/FetchCountries.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+function FetchCountries() {
+  const [countries, setCountries] = useState([]);
+
+  const getSortedCountries = () => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        data.sort((a, b) => {
+          const nameA = a.name.common.toUpperCase();
+          const nameB = b.name.common.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+    fetchCountries();
+  };
+  useEffect(getSortedCountries, []);
+
+  return (
+    <>
+      <option value="" disabled selected></option>
+      {countries.map((country) => (
+        <option key={country} value={country}>
+          {country.name.common}
+        </option>
+      ))}
+    </>
+  );
+}
+
+export { FetchCountries };
