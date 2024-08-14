fd1c9db4128959ee74353584697d93c96235996a
asdacosta <asdacosta18@gmail.com>
2024-08-14 18:44:49 +0000

    Add validation feedbacks

---
+++ src/Contact/feedback.js
@@ -0,0 +1,11 @@
+const feedbacks = {
+  name: [
+    "No special symbols allowed.",
+    "No numbers allowed.",
+    "Name must be two or more characters.",
+  ],
+  email: ["Enter a valid email."],
+  tel: ["Enter a valid phone number."],
+};
+
+export { feedbacks };
