96560f11a4fc47ee614319a9dfa981976bff9771
asdacosta <asdacosta18@gmail.com>
2024-08-15 23:08:01 +0000

    Clear trivial full stops

--- src/Contact/feedback.js
+++ src/Contact/feedback.js
@@ -1,11 +1,11 @@
 const allFeedbacks = {
   name: [
-    "No special symbols allowed.",
-    "No numbers allowed.",
-    "Name must be two or more characters.",
+    "No special symbols allowed",
+    "No numbers allowed",
+    "Name must be two or more characters",
   ],
-  email: ["Enter a valid email."],
-  tel: ["Enter a valid phone number or leave blank."],
+  email: ["Enter a valid email"],
+  tel: ["Enter a valid phone number or leave blank"],
 };
 
 export { allFeedbacks };
