ab14eea12a5c5b9f7d5a82945c99d99990ceed15
asdacosta <asdacosta18@gmail.com>
2024-08-16 23:37:17 +0000

    Remove unused string

--- src/Contact/feedback.js
+++ src/Contact/feedback.js
@@ -3,7 +3,6 @@ const allFeedbacks = {
     "No special symbols allowed",
     "No numbers allowed",
     "Must be two or more characters",
-    "Must not contain only whitespace",
   ],
   email: ["Enter a valid email"],
   tel: ["Enter a valid phone number or leave blank"],
