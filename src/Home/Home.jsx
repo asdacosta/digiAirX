44f52ca27379adb25d3945fd20a47f306c58431f
asdacosta <asdacosta18@gmail.com>
2024-08-19 23:48:57 +0000

    Make emoji size be same as font size

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -37,7 +37,7 @@ function Home() {
         "Need digital services? <span style='color: #0077b6; font-weight: 600;'>Brand Design</span>...",
         // "Need digital services? <span style='color: #0077b6; font-weight: 600;'>Video Production</span>...",
         // "Need digital services? <span style='color: #0077b6; font-weight: 600;'>Social Media Management</span>...",
-        'From minor to major projects, <br>our team of experts have you covered! <svg style="fill: #0077b6; width: 1.5rem;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm177.6 62.1C192.8 334.5 218.8 352 256 352s63.2-17.5 78.4-33.9c9-9.7 24.2-10.4 33.9-1.4s10.4 24.2 1.4 33.9c-22 23.8-60 49.4-113.6 49.4s-91.7-25.5-113.6-49.4c-9-9.7-8.4-24.9 1.4-33.9s24.9-8.4 33.9 1.4zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>',
+        'From minor to major projects, <br>our team of experts have you covered! <svg style="fill: #0077b6; width: 1.3rem;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm177.6 62.1C192.8 334.5 218.8 352 256 352s63.2-17.5 78.4-33.9c9-9.7 24.2-10.4 33.9-1.4s10.4 24.2 1.4 33.9c-22 23.8-60 49.4-113.6 49.4s-91.7-25.5-113.6-49.4c-9-9.7-8.4-24.9 1.4-33.9s24.9-8.4 33.9 1.4zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>',
       ],
       typeSpeed: 30,
       backSpeed: 15,
