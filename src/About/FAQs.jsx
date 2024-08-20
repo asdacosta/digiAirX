7ae0b55466ad263bf67d44e7d55832a87667d76c
asdacosta <asdacosta18@gmail.com>
2024-08-20 19:51:28 +0000

    Change name since extension is changed

--- src/About/FAQs.jsx
+++ src/About/FAQs.jsx
@@ -1,33 +1,52 @@
+import styles from "./About.module.css";
+import { Link } from "react-router-dom";
+
 const faqs = [
   {
     key: "faq1",
     question: "How do I get started?",
-    answer:
-      "To get started, simply reach out to us through our contact form, email, or phone. We'll schedule a consultation to understand your needs and recommend the best solutions tailored to your business.",
+    answer: (
+      <>
+        To get started, simply reach out to us through our contact form or phone
+        at{" "}
+        <Link to="/contact">
+          <strong className={styles.faqLink}>Contact</strong>
+        </Link>
+        . We'll respond within 24 hours to understand your needs and recommend
+        the best solutions tailored to your business.
+      </>
+    ),
   },
   {
     key: "faq2",
     question: "What industries do you work with?",
-    answer:
-      "We work with a variety of industries, including e-commerce, healthcare, finance, education, real estate, hospitality, and more. Our services are customizable to fit the unique needs of each sector.",
+    answer: (
+      <>
+        We work with all industries! Explore our Industry field at{" "}
+        <Link to="/contact">
+          <strong className={styles.faqLink}>Contact</strong>
+        </Link>{" "}
+        to see the full list.
+      </>
+    ),
   },
   {
     key: "faq3",
     question: "What makes your company different from others?",
     answer:
-      "We differentiate ourselves through our customized approach, attention to detail, and commitment to understanding your unique business needs. Our team combines industry expertise with innovative solutions to deliver outstanding results.",
+      "We stand out with our dedicated team of experienced experts who deliver professional, modern, and swift services that are unique to your business, ensuring you gain a competitive edge in your industry.",
   },
   {
     key: "faq4",
     question: "How long does it take to complete a project?",
     answer:
-      "The timeline for completing a project depends on the scope and complexity of the work. A simple website may take a few weeks, while larger projects like web apps or comprehensive branding strategies may take a few months. We provide estimated timelines after our initial consultation.",
+      "Project timelines vary based on scope and complexity. Simple websites typically take within a week, while larger projects such as web apps or branding strategies take a few weeks. We provide actual estimated timelines following our initial consultation.",
   },
   {
     key: "faq5",
     question: "What is the cost of your services?",
     answer:
-      "Costs vary depending on the services you require and the complexity of the project. After discussing your specific needs, we will provide a detailed quote.",
+      "Costs vary depending on the services you require and the complexity of the project. After discussing your specific needs or accepting our recommendations, we will provide a detailed invoice.",
   },
   {
     key: "faq6",
@@ -45,7 +64,7 @@ const faqs = [
     key: "faq8",
     question: "Do you offer ongoing support and maintenance?",
     answer:
-      "Absolutely! We offer post-launch support, ongoing maintenance, and updates for websites, web apps, and other digital assets to ensure they stay secure, up-to-date, and optimized for performance.",
+      "Absolutely! We offer ongoing support and maintenance to keep your websites, web apps, and digital assets secure, up-to-date, and optimized.",
   },
   // {
   //   key: "faq9",
@@ -57,25 +76,37 @@ const faqs = [
     key: "faq10",
     question: "How do you handle project communication?",
     answer:
-      "We use a combination of email, project management tools, and video conferencing to keep you informed throughout the process. Regular updates and checkpoints ensure transparency and smooth communication.",
+      "We use a combination of email, phone, and project management tools to keep you informed throughout the process.",
   },
   {
     key: "faq11",
-    question: "Do you offer custom solutions?",
-    answer:
-      "We offer both service packages and fully custom solutions tailored to the specific needs and budget of your business. During our consultation, we can recommend the best option for you.",
+    question: "Do you offer packages or custom solutions?",
+    answer: (
+      <>
+        We offer service packages through our{" "}
+        <Link to="/services">
+          <strong className={styles.faqLink}>Suite</strong>{" "}
+        </Link>
+        service, and custom solutions tailored to the specific needs and budget
+        of your business.{" "}
+        <Link to="/contact">
+          <strong className={styles.faqLink}>Reach out</strong>
+        </Link>{" "}
+        so we can recommend the best option for you.
+      </>
+    ),
   },
   {
     key: "faq12",
     question: "What is the difference between a web app and a website?",
     answer:
-      "A web app is a more interactive and complex tool designed for specific functions, often involving user input and dynamic content, while a website is generally more informational and static.",
+      "A web app is a more interactive and complex website designed for specific functions, often involving user input and dynamic content, while a website is generally more informational and static.",
   },
   {
     key: "faq13",
     question: "Do you offer multilingual website development?",
     answer:
-      "Yes, we can develop multilingual websites to cater to audiences in different languages, including language switching features and translation services.",
+      "Yes! We can develop multilingual websites to cater to audiences in different languages, including language switching features and translation services.",
   },
   {
     key: "faq14",
@@ -99,19 +130,19 @@ const faqs = [
     key: "faq17",
     question: "Can you refresh my existing brand identity?",
     answer:
-      "Yes! We can work with your existing brand to modernize and refine your visual identity while staying true to your core values.",
+      "Absolutely! We can work with your existing brand to modernize and refine your visual identity.",
   },
   {
     key: "faq18",
     question: "Can you design for both print and digital media?",
     answer:
-      "Yes, we design for both print and digital media, ensuring that your visual assets are optimized for their respective formats and deliver a consistent brand experience.",
+      "Yes! we design for both print and digital media, ensuring that your visual assets are optimized for their respective formats and deliver a consistent brand experience.",
   },
   {
     key: "faq19",
-    question: "Do you provide training on how to use the services you deliver?",
+    question: "Do you offer training for using your complex services?",
     answer:
-      "Yes, we offer training sessions and documentation to help you understand and effectively use the services and products we deliver.",
+      "Yes, we offer training sessions and documentation for complex services to help you understand and use them effectively.",
   },
   // {
   //   key: "faq20",
