4189f191e8d5873eaa8286a8deb58639efc8507b
asdacosta <asdacosta18@gmail.com>
2024-08-20 14:08:24 +0000

    Provide menu context for all components

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,15 @@ export const ThemeContext = createContext({
   setTheme: () => {},
 });
 
+export const MenuContext = createContext({
+  page: "",
+  setPage: () => {},
+});
+
 function App() {
   const [stopLoad, setStopLoad] = useState(false);
   const [theme, setTheme] = useState("light");
+  const [page, setPage] = useState("");
   const [extractedTheme, setExtractedTheme] = useState(false);
   const endLoadOnComplete = () => setStopLoad(true);
 
@@ -36,12 +42,12 @@ function App() {
       {!stopLoad ? (
         <Load endOnComplete={endLoadOnComplete} />
       ) : (
-        <>
+        <MenuContext.Provider value={{ page, setPage }}>
           <Background />
           <Nav />
           <Outlet />
           <Footer />
-        </>
+        </MenuContext.Provider>
       )}
     </ThemeContext.Provider>
   );
