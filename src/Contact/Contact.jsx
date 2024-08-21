5dfc03a2c796008b2b141d665d726175a0eecc78
asdacosta <asdacosta18@gmail.com>
2024-08-21 23:21:40 +0000

    Update texts

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -179,7 +179,9 @@ function Contact() {
               style={{ width: "8rem", height: "8rem" }}
             ></Lot>
           </div>
-          <span>Our team is available remotely 24/7 to meet your needs.</span>
+          <span>
+            Our team is available remotely 24/7 to meet all your needs.
+          </span>
         </div>
         <p>Expect a response within 24 hours after sending your inquiry.</p>
       </section>
@@ -432,7 +434,7 @@ function Contact() {
         </form>
       </section>
       <section className={styles.swiftContact}>
-        <h2>Reach out Swiftly</h2>
+        <h2>Reach out via Different Medium</h2>
         <section className={styles.swiftCard}>
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -444,7 +446,7 @@ function Contact() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
             </svg>
-            <span className={styles.swiftMail}>dacostasilvanus@gmail.com</span>
+            <span className={styles.swiftMail}>digiairx@gmail.com</span>
           </div>
         </section>
       </section>
